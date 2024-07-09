1eb62eb7965baa937f2be4b3d13d4c8d843637ed
Amine El Hassak <amel-has@e3r2p7.1337.ma>
2024-07-09 03:43:23 +0100

    update exercice 02

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -2,21 +2,21 @@
 
 int Account::getNbAccounts(void)
 {
-    
+    return (_nbAccounts);
 }
 int	Account::getTotalAmount( void )
 {
-
+    return (_totalAmount);
 }
 
 int	Account::getNbDeposits( void )
 {
-
+    return (_totalNbDeposits);
 }
 
 int	Account::getNbWithdrawals( void )
 {
-
+    return (_totalNbWithdrawals);
 }
 
 void    Account::displayAccountsInfos( void )
